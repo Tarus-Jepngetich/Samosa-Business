17b3e3266b976edbc1eaa05d67fd45708cbb1299
Tarus Jepngetich <tarjep99@gmail.com>
2026-03-05 17:43:13 +1000

    CORS Update

--- samosa-backend/server.js
+++ samosa-backend/server.js
@@ -19,7 +19,10 @@ mongoose.connection.once("open", async () => {
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }))
 app.use(express.json())
 app.use("/api/admin", require("./routes/adminAuth"))
 app.use("/api/payment", paymentRoutes)
